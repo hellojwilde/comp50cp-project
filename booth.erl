f95985a2715e7ce083fba5ab1db12f04a0c5d0b0
Josh Mermelstein <JoshMermel@gmail.com>
2014-11-06 03:47:52 -0500

    I should test more before pushing

--- booth.erl
+++ booth.erl
@@ -1,29 +1,33 @@
 -module(booth).
--export([loop/4, init/0]).
+-export([init/0]).
 
 init() ->
     spawn(fun() -> loop(sets:new(), [], [], maps:new()) end).
 
 loop(Not_yet_voted, Unbatched_ballots, Batched_ballots, Voting_schemes) ->
     receive
+        % register a new voter at this location
         {registration, Credentials} ->
             New_not_yet_voted = sets:add_element(Credentials, Not_yet_voted),
             loop(New_not_yet_voted, Unbatched_ballots, Batched_ballots, Voting_schemes);
+        % cast a vote
         {ballot, Credentials, Data} ->
             Valid_registration = sets:is_element(Credentials, Not_yet_voted),
             if
                 Valid_registration ->
                     New_not_yet_voted = sets:remove(Credentials, Not_yet_voted),
-                    loop(New_not_yet_voted, New_unbatched_ballots, Batched_ballots, Voting_schemes)
+                    New_unbatched_ballots = [Data | Unbatched_ballots],
+                    loop(New_not_yet_voted, New_unbatched_ballots, Batched_ballots, Voting_schemes);
                 true ->
                     bad_ballot
             end;
-        % new scheme addition
+        % add a new ballot tallying system
         {newScheme, Batchfun, Pid} ->
             New_schemes = maps:put(Pid, Batchfun, Voting_schemes),
             % resend all unbatched ballots
             Pid ! {batch, Batchfun(Batched_ballots)},
             loop(Not_yet_voted, Unbatched_ballots, Batched_ballots, New_schemes);
+        % send all unbatched data
         flush ->
             Fun = fun(Pid,Batchfun) -> Pid ! {batch, Batchfun(Unbatched_ballots)} end,
             maps:map(Fun, Voting_schemes),
