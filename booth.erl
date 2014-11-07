430d35cb1299c06510ab64b52e6470c7a9a83460
Josh Mermelstein <JoshMermel@gmail.com>
2014-11-07 02:46:11 -0500

    switched voting schemes to list from map.  Fixed bug where server would stop when a bad vote was received

--- booth.erl
+++ booth.erl
@@ -2,7 +2,7 @@
 -export([init/0]).
 
 init() ->
-    spawn(fun() -> loop(sets:new(), [], [], maps:new()) end).
+    spawn(fun() -> loop(sets:new(), [], [], []) end).
 
 loop(Not_yet_voted, Unbatched_ballots, Batched_ballots, Voting_schemes) ->
     receive
@@ -19,18 +19,18 @@ loop(Not_yet_voted, Unbatched_ballots, Batched_ballots, Voting_schemes) ->
                     New_unbatched_ballots = [Data | Unbatched_ballots],
                     loop(New_not_yet_voted, New_unbatched_ballots, Batched_ballots, Voting_schemes);
                 true ->
-                    bad_ballot
+                    loop(Not_yet_voted, Unbatched_ballots, Batched_ballots, Voting_schemes)
             end;
         % add a new ballot tallying system
         {newScheme, Batchfun, Pid} ->
-            New_schemes = maps:put(Pid, Batchfun, Voting_schemes),
+            New_schemes = [{Pid, Batchfun} | Voting_schemes],
             % resend all unbatched ballots
             Pid ! {batch, Batchfun(Batched_ballots)},
             loop(Not_yet_voted, Unbatched_ballots, Batched_ballots, New_schemes);
         % send all unbatched data
         flush ->
-            Fun = fun(Pid,Batchfun) -> Pid ! {batch, Batchfun(Unbatched_ballots)} end,
-            maps:map(Fun, Voting_schemes),
+            Fun = fun({Pid,Batchfun}) -> Pid ! {batch, Batchfun(Unbatched_ballots)} end,
+            lists:map(Fun, Voting_schemes),
             loop(Not_yet_voted, [], Batched_ballots ++ Unbatched_ballots, Voting_schemes);
         quit ->
             ok
