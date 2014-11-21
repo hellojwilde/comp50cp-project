9dcae01595fba3cd8cd1942deac6d3cae414f04f
Jonathan Wilde <hello@jwilde.me>
2014-11-21 17:33:26 -0500

    Adding default OTP supervisor magic. Josh, build on top of this.

---
+++ src/vote_app.erl
@@ -0,0 +1,16 @@
+-module(vote_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    vote_sup:start_link().
+
+stop(_State) ->
+    ok.
