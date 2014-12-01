f52d485e03a985c14dc6d97c2733a0597565d1c6
Josh Mermelstein <JoshMermel@gmail.com>
2014-11-30 23:41:19 -0500

    create_talliers now works but needs correct update functions

--- src/manager.erl
+++ src/manager.erl
@@ -1,5 +1,6 @@
 -module(manager).
 -export([init/0]).
+-include("../include/vote.hrl").
 
 % TODO:
 % everything related to webserver
@@ -32,16 +33,15 @@ loop(Registrar, Booths, Talliers) ->
         {newBooth, Booth} ->
             registrar:register_booth(Registrar, Booth),
             lists:map(fun(Tallier) -> Tallier ! {announce, Booth} end, Talliers),
-            loop(Registrar, Booths, Talliers, Serverdata);
+            loop(Registrar, Booths, Talliers);
         {newTallier, Tallier} ->
             lists:map(fun(Booth) -> Tallier ! {announce, Booth} end, Booths),
             % @JONATHAN tell webserver about it?
-            loop(Registrar, Booths, Talliers, Serverdata);
+            loop(Registrar, Booths, Talliers);
         flush ->
             lists:map(fun(Booth) -> Booth ! flush end, Booths),
-            loop(Registrar, Booths, Talliers, Serverdata);
+            loop(Registrar, Booths, Talliers);
         quit ->
-            % @JONATHAN do whatever webserver needs here
             ok
     end.
 
@@ -50,9 +50,9 @@ link_to_pid_list(Xs) ->
 
 create_talliers() ->
     [
-        tally:init(point_scheme:plurality(), ???),
-        tally:init(point_scheme:approval(), ???),
-        tally:init(point_scheme:borda_count(), ???),
-        tally:init(point_scheme:nauru(), ???),
-        tally:init(schultze, ???),
+        tally:init(point_scheme:plurality(), fun debug:update/1),
+        tally:init(point_scheme:approval(), fun debug:update/1),
+        tally:init(point_scheme:borda_count(), fun debug:update/1),
+        tally:init(point_scheme:nauru(), fun debug:update/1),
+        tally:init(schultze, fun debug:update/1)
     ].
